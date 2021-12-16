fa1f6a420820b00bb0af871da876a841e29867b7
Jaidev Chattre <jaidevchattre@gmail.com>
2021-12-16 08:49:04 +0530

    Using @ConfigurationProperties to read .yml properties

---
+++ twitter-to-kafka-service/src/test/java/com/microservices/demo/twitter/to/kafka/service/test/TwitterToKafkaServiceApplicationTest.java
@@ -0,0 +1,15 @@
+package com.microservices.demo.twitter.to.kafka.service.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TwitterToKafkaServiceApplicationTest {
+
+//    @Test
+//    public void contextLoad(){
+//
+//    }
+}
